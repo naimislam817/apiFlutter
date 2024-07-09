367ac374a1af6f72adf0211258c7e5d1c681b158
naimislam817 <naimislam231@gmail.com>
2024-07-10 00:59:37 +0600

    ApiCodes

---
+++ lib/example_fiverr.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class LastExampleScreen extends StatefulWidget {
+  const LastExampleScreen({super.key});
+
+  @override
+  State<LastExampleScreen> createState() => _LastExampleScreenState();
+}
+
+class _LastExampleScreenState extends State<LastExampleScreen> {
+
+   Future<void<>>
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Api Course'),
+      ),
+      body:  Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
